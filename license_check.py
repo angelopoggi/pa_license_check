4cbc5ff79ef363ec7ad37b7aeca2959c63292d9f
Angelo Poggi <angelopoggi@Angelo-Poggi-Remote-Laptop.local>
2021-02-09 09:54:56 -0500

    Fixed config parser - expanded on date logic

--- license_check.py
+++ license_check.py
@@ -2,22 +2,29 @@
 import requests
 import xml.etree.ElementTree as ET
 from dateutil.parser import parse
-import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
 import click
+from configparser import ConfigParser
 
 @click.command()
-@click.option('--fw', help='The FQDN of the firewall')
-@click.option('--key', help='the unique key for the firewall')
-def lc_check(fw,key):
+@click.option('--client', help='Client name as defined in the fw_key.ini file')
+def lc_check(client):
+    fw_config = ConfigParser()
+    fw_config.read('fw_key.ini')
+    selected_fw = fw_config.get(section=client, option='fw')
+    selected_key = fw_config.get(section=client, option='key')
+
     """This Script will check the specified firewall and determine its license status. You must generate an API key before usage"""
-    url = f"https://{fw}/api/?type=op&cmd=<request><license><info></info></license></request>&key={key}"
+    url = f"https://{selected_fw}/api/?type=op&cmd=<request><license><info></info></license></request>&key={selected_key}"
     try:
         response = requests.get(url, verify=False).content
     except:
-        print(f"{fw} is not reachable, please check spelling or availability")
+        print(f"{selected_fw} is not reachable, please check spelling or availability")
     xml_response = ET.fromstring(response)
 
-    todays_date = datetime.datetime.now().date()
+    expirationWindow = datetime.now().date() + timedelta(days=60)
+
 
     for item in xml_response.iter():
         if 'feature' in item.tag:
@@ -25,7 +32,8 @@ def lc_check(fw,key):
         elif 'expires' in item.tag and 'Never' not in item.text:
             expires = item.text
             expires_time = parse(expires).date()
-            if todays_date >= expires_time:
+
+            if expirationWindow >= expires_time:
                 print(f"feature: {feature} has expired. Expiration date was {expires_time}")
             else:
                 continue
